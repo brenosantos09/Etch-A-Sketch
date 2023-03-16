3f2f7545db355fc9e70f1e2302415bddc1a885f0
Breno Santos <brenowillian0357@gmail.com>
2023-03-16 18:18:43 -0300

    added clear grid functionality and function for drawing

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ for (let i = 0; i < 256; i++) {
   const gridCells = document.createElement('div');
   gridCells.classList.add('grid-cell');
   sketchBoard.append(gridCells);
+  drawOnSketch(gridCells);
+  clearBoard(gridCells);
 }
 
 //generate new grid
@@ -24,11 +26,28 @@ newGridButton.addEventListener('click', () => {
   } else {
     sketchBoard.innerHTML = '';
     for (let i = 0; i < newGridSize; i++) {
-      const newGridCell = document.createElement('div');
-      newGridCell.classList.add('grid-cell');
-      sketchBoard.append(newGridCell);
+      const newGridCells = document.createElement('div');
+      newGridCells.classList.add('grid-cell');
+      sketchBoard.append(newGridCells);
+      drawOnSketch(newGridCells);
+      clearBoard(newGridCells);
     }
     sketchBoard.style.gridTemplateColumns = `repeat(${newGrid}, 1fr)`;
     sketchBoard.style.gridTemplateRows = `repeat(${newGrid}, 1fr)`;
   }
 });
+
+//clear grid
+function clearBoard(cells) {
+  clearButton.addEventListener('click', () => {
+    cells.style.backgroundColor = 'rgb(135, 206, 250)';
+  });
+}
+
+//draw on sketch
+
+function drawOnSketch(cells) {
+  cells.addEventListener('mouseover', () => {
+    cells.style.backgroundColor = '#000';
+  });
+}
